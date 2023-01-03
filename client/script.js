a2048f8d1260cf8461a1f3abf1bbf5961325e158
Olowookere Temitope Johnson <olowott@gmail.com>
2023-01-03 08:26:27 +0100

    add render.com server link

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
   //fetch data from the server
-  const response = await fetch('http://localhost:5500', {
+  const response = await fetch('https://openai-codex-lit3.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
